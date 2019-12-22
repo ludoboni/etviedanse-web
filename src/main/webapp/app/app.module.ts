c7a66349a68ba6b733739ea990b11e175f8935d1
ludoboni <leoboni31@gmail.com>
2019-12-22 17:06:34 +0100

    clean

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -16,7 +16,6 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
-import { CarouselComponent } from './ui-components/carousel/carousel.component';
 
 @NgModule({
   imports: [
@@ -30,15 +29,7 @@ import { CarouselComponent } from './ui-components/carousel/carousel.component';
     EtviedanseEntityModule,
     EtviedanseAppRoutingModule
   ],
-  declarations: [
-    JhiMainComponent,
-    NavbarComponent,
-    ErrorComponent,
-    PageRibbonComponent,
-    ActiveMenuDirective,
-    FooterComponent,
-    CarouselComponent
-  ],
+  declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
   bootstrap: [JhiMainComponent]
 })
 export class EtviedanseAppModule {}
