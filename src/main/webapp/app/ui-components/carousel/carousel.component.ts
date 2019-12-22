517544483cff3714cdb0e8b0129269145cec742b
ludoboni <leoboni31@gmail.com>
2019-12-22 14:28:08 +0100

    create carousel

---
+++ src/main/webapp/app/ui-components/carousel/carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
